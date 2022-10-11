0d14aa4753d8e757a641539fefc4061eaeda6461
KIM_TAE_WON <eyesings@radcns.com>
2022-10-11 16:55:03 +0900

    UI 구성 완료

---
+++ UberClone/UberClone/View/LocationCell.swift
@@ -0,0 +1,50 @@
+//
+//  LocationCell.swift
+//  UberClone
+//
+//  Created by RadCns_KIM_TAEWON on 2022/10/11.
+//
+
+import UIKit
+
+class LocationCell: UITableViewCell {
+
+    // MARK: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "123 Main Street"
+        return label
+    }()
+    
+    private let addressLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.textColor = .lightGray
+        label.text = "123 Main Street, Washington, DC"
+        return label
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        selectionStyle = .none
+        
+        let stack = UIStackView(arrangedSubviews: [titleLabel, addressLabel])
+        stack.axis = .vertical
+        stack.distribution = .fillEqually
+        stack.spacing = 4
+        
+        addSubview(stack)
+        stack.centerY(inView: self, leftAnchor: leftAnchor, paddingLeft: 12)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
